961dbd3e7a991060f3f95719de6cfd0f9df83d5e
Nazimcp <75988681+Nazimcp@users.noreply.github.com>
2021-01-01 10:05:57 +0530

    Create wordlist.sh

---
+++ wordlist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sleep 2
+figlet NAZIM
+sleep 6
+echo $blue." PLEASE FOLLOW ME ON INSTAGRAM AND FACEBOOK";
+sleep 4
+echo $Red." GENARATING WORDLIST FOR YOU";                                                                                            
+sleep 4
+                                                                     
+
+string=( {a..z} )
+for x in ${string[@]}; do
+for y in ${string[@]}; do
+for z in ${string[@]}; do
+for w in ${string[@]}; do
+for k in ${string[@]}; do
+#for q in ${string[@]}; do
+echo $x$y$z$w$k
+
+done done done done done
